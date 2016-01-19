62002b9f3fdd2f1fec744808841682c686cabbe0
Larry <larry.fenn@gmail.com>
2016-01-18 23:12:05 -0500

    initial commit

---
+++ Delay/Delay/DelayTool.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using Un4seen.Bass;
+using Un4seen.BassWasapi;
+
+namespace Delay
+{
+    public class DelayTool
+    {
+        private ComboBox inlist;
+        private ComboBox outlist;
+        private bool initialized;
+        public DelayTool(ComboBox inlist, ComboBox outlist)
+        {
+            BassNet.Registration("larry.fenn@gmail.com", "2X531420152222");
+            this.inlist = inlist;
+            this.outlist = outlist;
+            initialized = false;
+            init();
+        }
+        private void init()
+        {
+            bool result = false;
+            for (int i = 0; i < BassWasapi.BASS_WASAPI_GetDeviceCount(); i++)
+            {
+                var device = BassWasapi.BASS_WASAPI_GetDeviceInfo(i);
+                if (device.IsEnabled && device.IsLoopback)
+                {
+                    inlist.Items.Add(string.Format("{0} - {1}", i, device.name));
+                    outlist.Items.Add(string.Format("{0} - {1}", i, device.name));
+                }
+            }
+            inlist.SelectedIndex = 0;
+            outlist.SelectedIndex = 0;
+            Bass.BASS_SetConfig(BASSConfig.BASS_CONFIG_UPDATETHREADS, false);
+            // 'No sound' device for double buffering: BASS_WASAPI_BUFFER flag requirement
+            result = Bass.BASS_Init(0, 44100, BASSInit.BASS_DEVICE_DEFAULT, IntPtr.Zero);
+            if (!result) throw new Exception("Init Error");
+        }
+    }
+}
---
+++ Delay/Delay/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Delay
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private bool dragStarted = false;
+        private double delay = 0;
+        private DelayTool dt;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            dt = new DelayTool(sourceList, outList);
+        }
+
+        private void Slider_DragCompleted(object sender, DragCompletedEventArgs e)
+        {
+            set(((Slider)sender).Value);
+            this.dragStarted = false;
+        }
+
+        private void Slider_DragStarted(object sender, DragStartedEventArgs e)
+        {
+            this.dragStarted = true;
+        }
+
+        private void Slider_ValueChanged(
+            object sender,
+            RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (!dragStarted)
+                set(e.NewValue);
+        }
+
+        private void set(double val)
+        {
+            delay = val;
+            label.Content = Math.Round(10*delay) + "ms";
+        }
+    }
+}
